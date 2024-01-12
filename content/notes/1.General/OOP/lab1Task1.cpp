d75722e244b7f9be84c7406cb43b7b07417d5e8e
harcolemas@gmail.com <harcolemas@gmail.com>
2024-01-13 01:52:55 +0500

    Quartz sync: Jan 13, 2024, 1:52 AM

--- content/notes/1.General/OOP/lab1Task1.cpp
+++ content/notes/1.General/OOP/lab1Task1.cpp
@@ -1,56 +1,29 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-class Cart{
+class Employee {
+  char* empName;
+  const int empID;
   public:
-  int quantity;
-  float priceOfBag = 5.5;
-  void setQuantity(int q){
-    quantity = q;
-  }
-};
-
-class Bill{
-  private:
-    int lBoxCap = 20;
-    float lBoxCost = 1.8;
-    int mBoxCap = 10;
-    float mBoxCost = 1.0;
-    int sBoxCap = 5;
-    float sBoxCost = 0.6;
-    float totalCost = 0;
-  public:
-    void calculateBill(Cart c){
-      int quantity = c.quantity;
-      if (quantity >= lBoxCap){
-       std::cout << quantity/lBoxCap << "Large Boxes: " << (int)(quantity/lBoxCap)*lBoxCost << std::endl;
-       totalCost += (int)(quantity/lBoxCap)*lBoxCost;
-       quantity = quantity % lBoxCap;
-      }
-      if (quantity >= mBoxCap){
-       std::cout << quantity/mBoxCap << "Medium Boxes: " << (int)(quantity/mBoxCap)*mBoxCost << std::endl;
-       totalCost += (int)(quantity/mBoxCap)*mBoxCost;
-       quantity = quantity % mBoxCap;
-       // cout << quantity << endl;
-      }
-      if (quantity >= 0){
-        int sBox = ceil((float) quantity/sBoxCap);
-       std::cout << sBox << "Small Boxes: " << sBox*sBoxCost << std::endl;
-       totalCost += sBox*sBoxCost;
-      }
-
-      cout << "Total Cost: " << totalCost + c.quantity*c.priceOfBag << endl;
-
+    Employee(char* name, int id):empID(id)
+    {
+      empName = name;
+    };
+    
+    void setName(char* name){
+      empName = name;
+    }
+    void getData(){
+      cout << "Name: " << empName << "\nID: " << empID << endl;
     }
 };
 
 int main (int argc, char *argv[]) {
-  Bill b;
-  Cart c;
-    c.quantity = 52;
-  std::cout << "The Cost of Order: " << c.quantity * c.priceOfBag << std::endl;
-  b.calculateBill(c);
+  Employee e1("Rajiv", 1), e2("Raj", 2), e3("Rajiv", 3);
+
+  e1.getData();
+  e1.setName("SHIT");
+  e1.getData();
 
   return 0;
 }
